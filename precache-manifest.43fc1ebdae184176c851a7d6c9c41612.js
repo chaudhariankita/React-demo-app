393a8cb8d2abd9d051fdbc0738fd559eff0053f9
chaudhariankita <58285009+chaudhariankita@users.noreply.github.com>
2020-08-21 19:02:46 +0530

    Updates

---
+++ precache-manifest.43fc1ebdae184176c851a7d6c9c41612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2831159021078fa3c15af77662ffd37a",
+    "url": "/React-demo-app/index.html"
+  },
+  {
+    "revision": "83c7d391721c1e21d221",
+    "url": "/React-demo-app/static/css/main.e57b6200.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-demo-app/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-demo-app/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c7d391721c1e21d221",
+    "url": "/React-demo-app/static/js/main.7bde868b.chunk.js"
+  },
+  {
+    "revision": "c4c34320994192a0fa6f",
+    "url": "/React-demo-app/static/js/runtime-main.601bb948.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
